4e9f04cfa3f7bd7029d2490b97cf9bdbc309b38c
Corbin HOPPER <choppe1@ds2.CS.McGill.CA>
2018-06-26 14:59:20 -0400

    ..

--- run_rupert.sh
+++ run_rupert.sh
@@ -6,4 +6,4 @@ if [ "$#" -ne 2 ]; then
     echo "Illegal number of parameters. Need #threads, then input_file."
 fi
 
-nohup mpirun --mca mpi_warn_on_fork 0 -n $1 python3 /home/2014/choppe1/Documents/TopologyEvolution/src/roots/evolve_rupert.py /home/2014/choppe1/Documents/TopologyEvolution/data/input/$2 > /home/2014/choppe1/Documents/TopologyEvolution/data/output/slog &
+nohup mpirun --mca mpi_warn_on_fork 0 -n $1 python3 /home/2014/choppe1/Documents/TopologyEvolution/src/evolve.py /home/2014/choppe1/Documents/TopologyEvolution/data/input/$2 > /home/2014/choppe1/Documents/TopologyEvolution/data/output/slog &
